120ec88b4c45514ffb0e5b553655ccedd10563a9
isaka1022 <isaka1022@gmail.com>
2025-03-01 12:35:16 +0900

    テストごとにviewsのパスを変更するようにする

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require 'rspec'
 require 'rack/test'
 require 'capybara/rspec'
-require 'sinatra'
 # db/todosファイルが存在する場合のみ読み込む
 if File.exist?(File.expand_path('../db/todos.rb', __dir__))
   require_relative '../db/todos'
@@ -19,28 +18,27 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-  config.before(:all) do
-    Sinatra::Application.set :views, File.join(File.dirname(__FILE__), '../views')
-  end
-  
   config.before(:suite) do
-    @original_stdout = $stdout
-    @original_stderr = $stderr
+    # グローバル変数を使用して標準出力と標準エラー出力を保存
+    $original_stdout = $stdout
+    $original_stderr = $stderr
     $stdout = File.open(File::NULL, "w")
     $stderr = File.open(File::NULL, "w")
   end
 
-  after(:suite) do
-    $stdout = @original_stdout
-    $stderr = @original_stderr
+  config.after(:suite) do
+    $stdout = $original_stdout
+    $stderr = $original_stderr
   end
 
-  config.before(:each) do
-    Capybara.app = Sinatra::Application
-    Capybara.default_max_wait_time = 5  # 待機時間を5秒に設定
+  config.before(:each) do |example|
+    if defined?(Sinatra::Application) && example.metadata[:require_sinatra] != false
+      Sinatra::Application.set :views, File.join(File.dirname(__FILE__), '../views')
+      Capybara.app = Sinatra::Application
+      Capybara.default_max_wait_time = 5  # 待機時間を5秒に設定
+    end
   end
 
   config.before(:each, clear_todos: true) do
