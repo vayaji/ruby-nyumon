f6bee670f06cd508c4262a0e24e48b2bc3904cfb
vayaji <176355251+vayaji@users.noreply.github.com>
2025-06-06 16:12:48 +0900

    10

--- app.rb
+++ app.rb
@@ -18,6 +18,11 @@ end
 
 get '/todos/:id/edit' do
   @todo = DB.execute('SELECT * FROM todos WHERE id = ?', params[:id]).first
-  halt 404 unless @todo
+  halt 404, 'Todo not found!' unless @todo
   erb :edit
+end
+
+put '/todos/:id' do
+  DB.execute('UPDATE todos SET title = ? WHERE id = ?', [params[:title], params[:id]])
+  redirect '/todos'
 end
